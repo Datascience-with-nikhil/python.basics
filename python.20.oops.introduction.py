6495984bd1c5758aee9e27317687c82237fd685b
Datascience-with-nikhil <nikhil882002verma@gmail.com>
2024-09-20 22:38:43 +0530

    python oops introduction

---
+++ python.20.oops.introduction.py
@@ -0,0 +1,34 @@
+print("introduction of oops")
+print("creating a class")
+
+a = 1
+print(type(a))
+
+print(type(1))
+
+
+class test:
+    pass
+
+
+a1 = test()
+
+print(type(test))
+
+print(type(a1))
+
+
+class test1:
+    pass
+
+
+print()
+class soft_drinks:
+
+    def fat(self):
+        print("in soft present dangers chemicals")
+
+
+person_1 = soft_drinks()
+
+person_1.fat()
